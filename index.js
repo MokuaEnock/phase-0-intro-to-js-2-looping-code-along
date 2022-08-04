cd44ee14bce6f30ecdc64deeb909dc1a14af2d28
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-08-04 11:19:25 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+function countDown(n) {
+  let num = [];
+  for (let i = 0; i <= n; i += 1) {
+    num.unshift(i);
+  }
+  for (let i of num) {
+    console.log(i);
+  }
+}
+
+function writeCards(n, k) {
+  let names = [];
+  for (let i of n) {
+    let mes = `Thank you, ${i}, for the wonderful ${k} gift!`;
+    names.push(mes);
+  }
+  return names;
+}
+writeCards(["enock", "mokua"], "sad")
